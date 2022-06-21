9a2c94d81e17f48154d1d8abc0f909a4145c8fb0
Jimmy <jimmyn.kiarie@gmail.com>
2022-06-21 21:08:44 +0300

    configuring terraform file

--- stage_2/main.tf
+++ stage_2/main.tf
@@ -1,3 +1,11 @@
+locals {
+  project_id     = "fluted-ranger-351208"
+  network        = "default"
+  image          = "ubuntu-pro-2004-focal-v20220610"
+  ssh_user       = "root@srv001"
+  private_key_path = "~/.ssh/ansible_ed25519"
+}
+
 #Definition
 terraform {
   required_providers {
@@ -12,18 +20,44 @@ terraform {
 provider "google" {
   # Configuration options
   credentials = file("./CREDENTIALS_FILE.json")
-  project = "fluted-ranger-351208"
-  region = "us-central1 (Iowa)"
-  zone         = "us-central1-c"
+  project = local.project_id
+  region  = "us-central1"
+  zone    = "us-central1-a"
+
 }
+
+# Define Service Account tp grant permission to apps on your VM
+resource "google_service_account" "default" {
+  account_id = "113008182886204786185"
+}
+
+# Define the port to be exposed
+resource "google_compute_firewall" "web" {
+  name = "web-access"
+  network = local.network
+
+  allow {
+    protocol = "tcp"
+    ports = ["3000"]
+  }
+
+  source_ranges          = [ "0.0.0.0/0" ]
+  target_service_accounts = [ "1075600608145-compute@developer.gserviceaccount.com" ]
+}
+
 
 #Resource Definition
 
-resource "google_compute_instance" "tf-ip3-server" {
-  name         = "tf-ip3-server"
-  machine_type = "f1-micro"
- 
+resource "google_compute_instance" "ip3_machines" {
+   count = length(var.new_machines)
+  name         = var.new_machines[count.index]
+  machine_type = "e2-medium"
 
+  named_port {
+    name = "http"
+    port = 3000
+  }
+ 
   tags = ["foo", "bar"]
 
   boot_disk {
@@ -51,3 +85,7 @@ resource "google_compute_instance" "tf-ip3-server" {
 
   metadata_startup_script = "echo hi > /test.txt"
 }
+
+provisioner "local-exec" {
+  command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u {var.user} -i '${self.ipv4_address},' --private-key ${var.ssh_private_key} playbook.yml"
+  }
---
+++ stage_2/variables.tf
@@ -0,0 +1,4 @@
+variable "new_machines" {
+  type = list(string)
+  default = [ "tech001", "finance001", "marketing001" ]
+}
